f4927dfe6571bc5f01266994f17b377b402982cf
kit2422mmc061-lgtm <kit.24.22mmc061@gmail.com>
2026-03-25 15:12:27 +0530

    demotest

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ const sum = num1 + num2;
 
 // display the sum
 console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+
+// store input numbers
+const num12 = parseInt(prompt('Enter the first number '));
+const num24 = parseInt(prompt('Enter the second number '));
+
+//add two numbers
+const sum1 = num1 + num2;
+
+// display the sum
+console.log(`The sum of ${num1} and ${num2} is ${sum}`);
